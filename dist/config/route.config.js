5287b12e26811bf1b1f989844caca87727a82c8f
Eniola Caleb Olagbegi <eniolaolagbegi@gmail.com>
2025-05-07 13:16:20 +0100

    Delete dist directory

--- dist/config/route.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RouteVersion = void 0;
-var RouteVersion;
-(function (RouteVersion) {
-    RouteVersion["v1"] = "v1";
-})(RouteVersion || (exports.RouteVersion = RouteVersion = {}));
