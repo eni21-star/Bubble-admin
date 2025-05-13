3c3f8941c3af53b53c3caf295cecea9868ff29db
Adesokan Adesayo <102504953+sayoadesokan@users.noreply.github.com>
2025-05-13 21:28:18 +0100

    delete users profile

--- src/v1/modules/admin/controllers/profile.controllers.ts
+++ src/v1/modules/admin/controllers/profile.controllers.ts
@@ -1,26 +1,45 @@
-import { NextFunction, Request, Response } from "express"
-import { ReqAdmin } from "../../../../shared/types/req.types"
-import { inject, injectable } from "tsyringe"
-import ProfileServices from "../services/profile.services"
-import { SuccessResponse } from "../../../../shared/utils/response.utils"
+import { NextFunction, Request, Response } from "express";
+import { ReqAdmin } from "../../../../shared/types/req.types";
+import { inject, injectable } from "tsyringe";
+import ProfileServices from "../services/profile.services";
+import { SuccessResponse } from "../../../../shared/utils/response.utils";
 
 @injectable()
-class ProfileController { 
+class ProfileController {
+  constructor(
+    @inject(ProfileServices) private profileServices: ProfileServices
+  ) {}
+  async getProfile(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const admin = (req as any).admin as ReqAdmin;
 
-    constructor(@inject(ProfileServices) private profileServices: ProfileServices){}
-    async getProfile(req: Request, res: Response, next: NextFunction): Promise<any>{
-        
-        try {
-
-            const admin = (req as any).admin as ReqAdmin
-
-            const response = await this.profileServices.getProfile(admin)
-            return res.status(200).json(SuccessResponse('Profile retrieved', response))
-            
-        } catch (error) {
-            next(error)
-        }
+      const response = await this.profileServices.getProfile(admin);
+      return res
+        .status(200)
+        .json(SuccessResponse("Profile retrieved", response));
+    } catch (error) {
+      next(error);
     }
+  }
+
+  async deleteProfile(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    try {
+      const { id } = req.params;
+
+      await this.profileServices.deleteProfile(id);
+      return res.status(200).json(SuccessResponse("Profile deleted retrieved"));
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
-export default ProfileController
+export default ProfileController;
--- src/v1/modules/admin/services/auth.services.ts
+++ src/v1/modules/admin/services/auth.services.ts
@@ -1,74 +1,87 @@
 import { inject, injectable } from "tsyringe";
 import { bcryptCompare, bcryptHash } from "../../../../shared/utils/hash.utils";
-import { signAccessToken, signRefreshToken } from "../../../../shared/utils/jwt.utils";
+import {
+  signAccessToken,
+  signRefreshToken,
+} from "../../../../shared/utils/jwt.utils";
 import RefreshToken from "../../../../database/entities/refresh-token.entities";
-import { defaultPermissions, superAdminPermissions } from "../../../../config/permissions.config";
+import {
+  defaultPermissions,
+  superAdminPermissions,
+} from "../../../../config/permissions.config";
 import { logger } from "../../../../shared/utils/logger/logger";
 import { LoginDto, RegisterDto } from "../dto/auth.dto";
 import AuthDatasource from "../datasource/auth.datasource";
-import { ConflictError, NotFoundError, UnauthorizedError } from "../../../../shared/errors/errors";
+import {
+  ConflictError,
+  NotFoundError,
+  UnauthorizedError,
+} from "../../../../shared/errors/errors";
 import RefreshTokenDatasource from "../datasource/refreshToken.datasource";
-
-
 
 @injectable()
 class AuthService {
+  constructor(
+    @inject(AuthDatasource) private authDatasource: AuthDatasource,
+    @inject(RefreshTokenDatasource)
+    private refreshTokenDatasource: RefreshTokenDatasource
+  ) {}
+  async register(data: RegisterDto): Promise<any> {
+    try {
+      let { email, username, password, role } = data;
+      const userExist = await this.authDatasource.findByEmail(email);
+      if (userExist)
+        throw new ConflictError(
+          "Admin with that email already exists. Try loggin in."
+        );
 
-    constructor(
-        @inject(AuthDatasource) private authDatasource: AuthDatasource,
-        @inject(RefreshTokenDatasource) private refreshTokenDatasource: RefreshTokenDatasource
-    ){}
-    async register(data: RegisterDto): Promise<any> {
-        try {
+      data.password = await bcryptHash(password);
+      data.email = email.toLowerCase();
+      logger.info(role);
 
-            let {email, username, password, role} = data
-            const userExist = await this.authDatasource.findByEmail(email)
-            if(userExist) throw new ConflictError('Admin with that email already exists. Try loggin in.')
+      if (role == "ADMIN") {
+        data.permissions = defaultPermissions;
+      } else if (role == "SUPPORT") {
+        data.isAvailable = true;
+        data.permissions = [];
+      } else {
+        data.permissions = superAdminPermissions;
+      }
 
-            data.password = await bcryptHash(password)
-            data.email = email.toLowerCase()
-            logger.info(role)
-
-
-            if(role == 'ADMIN') {
-                data.permissions = defaultPermissions 
-            }
-            else if (role == 'SUPPORT'){
-                data.isAvailable = true
-                data.permissions = []
-            }
-            else {
-                data.permissions = superAdminPermissions 
-            }
-          
-            const create = await this.authDatasource.newAdmin(data)
-            return create
-        } catch (error) {
-            throw error
-        }
+      const create = await this.authDatasource.newAdmin(data);
+      return create;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async login(data: LoginDto){
-        try {
+  async login(data: LoginDto) {
+    try {
+      const { email, password } = data;
+      const userExist = await this.authDatasource.findByEmail(email);
+      if (!userExist)
+        throw new NotFoundError(
+          "user does not exist. Please create an account"
+        );
 
-            const { email, password } = data
-            const userExist = await this.authDatasource.findByEmail(email)
-            if(!userExist) throw new NotFoundError('user does not exist. Please create an account')
-            
-            const compare = await bcryptCompare(password, userExist.password)
-            if(!compare) throw new UnauthorizedError('Incorrect password')
-            
-            const accessToken =  signAccessToken({id: userExist.id, permissions: userExist.permissions as string[]})
-            const refreshToken = signRefreshToken({id: userExist.id})
+      const compare = await bcryptCompare(password, userExist.password);
+      if (!compare) throw new UnauthorizedError("Incorrect password");
 
-            const saveRefresh = await this.refreshTokenDatasource.saveRefreshToken(userExist, refreshToken)
-            return { accessToken, refreshToken }
-            
-        } catch (error) {
-            throw error 
-        }
+      const accessToken = signAccessToken({
+        id: userExist.id,
+        permissions: userExist.permissions as string[],
+      });
+      const refreshToken = signRefreshToken({ id: userExist.id });
+
+      const saveRefresh = await this.refreshTokenDatasource.saveRefreshToken(
+        userExist,
+        refreshToken
+      );
+      return { accessToken, refreshToken };
+    } catch (error) {
+      throw error;
     }
+  }
 }
 
-
-export default AuthService
+export default AuthService;
