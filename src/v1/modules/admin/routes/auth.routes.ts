f0e9e50414c4daf5f1bcd60ce2875de27332525d
Adesokan Adesayo <102504953+sayoadesokan@users.noreply.github.com>
2025-05-13 21:28:18 +0100

    delete users profile

--- src/v1/modules/admin/routes/auth.routes.ts
+++ src/v1/modules/admin/routes/auth.routes.ts
@@ -1,21 +1,22 @@
-import express from 'express'
-const authRouter = express.Router()
-import { container } from 'tsyringe'
-import { reqValidator } from '../../../../shared/middleware/validation.middleware'
-import { LoginDto, RegisterDto } from '../dto/auth.dto'
-import AuthController from '../controllers/auth.controllers'
+import express from "express";
+const authRouter = express.Router();
+import { container } from "tsyringe";
+import { reqValidator } from "../../../../shared/middleware/validation.middleware";
+import { LoginDto, RegisterDto } from "../dto/auth.dto";
+import AuthController from "../controllers/auth.controllers";
 
-const authController = container.resolve(AuthController)
+const authController = container.resolve(AuthController);
 
 authRouter
-.post('/register', reqValidator(RegisterDto), authController.register.bind(authController))
-.post('/login', reqValidator(LoginDto), authController.login.bind(authController))
+  .post(
+    "/register",
+    reqValidator(RegisterDto),
+    authController.register.bind(authController)
+  )
+  .post(
+    "/login",
+    reqValidator(LoginDto),
+    authController.login.bind(authController)
+  );
 
-
-
-
-
-
-
-
-export default authRouter
+export default authRouter;
--- src/v1/modules/admin/services/profile.services.ts
+++ src/v1/modules/admin/services/profile.services.ts
@@ -1,27 +1,34 @@
-import { inject, injectable } from "tsyringe"
-import AuthDatasource from "../datasource/auth.datasource"
-import { ReqAdmin } from "../../../../shared/types/req.types"
-import { NotFoundError } from "../../../../shared/errors/errors"
+import { inject, injectable } from "tsyringe";
+import AuthDatasource from "../datasource/auth.datasource";
+import { ReqAdmin } from "../../../../shared/types/req.types";
+import { NotFoundError } from "../../../../shared/errors/errors";
 
 @injectable()
 class ProfileServices {
+  constructor(@inject(AuthDatasource) private authDatasource: AuthDatasource) {}
+  async getProfile(admin: ReqAdmin) {
+    try {
+      const { id } = admin;
+      const userExist = await this.authDatasource.findByIdProfile(id);
+      if (!userExist) throw new NotFoundError("User does not exist.");
 
-    constructor(
-        @inject(AuthDatasource) private authDatasource:AuthDatasource,
-     ){}
-    async getProfile(admin: ReqAdmin){
-        try {
-            
-            const { id } = admin
-            const userExist = await this.authDatasource.findByIdProfile(id)
-            if(!userExist) throw new NotFoundError('User does not exist.')
-
-            return userExist
-
-        } catch (error) {
-            throw error
-        }
+      return userExist;
+    } catch (error) {
+      throw error;
     }
+  }
+
+  async deleteProfile(admin: ReqAdmin) {
+    try {
+      const { id } = admin;
+      const userExist = await this.authDatasource.findByIdProfile(id);
+      if (!userExist) throw new NotFoundError("User does not exist.");
+
+      return await this.authDatasource.deletAdminById(id);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-export default ProfileServices
+export default ProfileServices;
