ea3b030fbb58a2836408f091a5b9e7c727acae74
Leb <eniolaolagbegi@gmail.com>
2025-06-06 16:39:53 +0100

    staging

--- src/v1/modules/support/routes/support.routes.ts
+++ src/v1/modules/support/routes/support.routes.ts
@@ -13,7 +13,7 @@ newTicketRouter
 .post('/support/admin-reply', [authMiddleware, reqValidator(AdminTicketMessageDto)], supportController.adminReplyToTicket.bind(supportController))
 .post('/support/user-reply', reqValidator(UserTicketMessageDto), supportController.userReplyToTicket.bind(supportController))
 .post('/support/resolve-ticket', [authMiddleware, reqValidator(ResolveTicketDto)], supportController.resolveTicket.bind(supportController))
-.get(`/support/get-ticket/:id`, authMiddleware,  supportController.getTicket.bind(supportController))
+.get(`/support/get-ticket/:id`,   supportController.getTicket.bind(supportController))
 .post('/support/assign-admin', [authMiddleware, reqValidator(ResolveTicketDto)], supportController.assignAdmin.bind(supportController))
 .get('/support/get-tickets', authMiddleware, supportController.getAdminTicketHistory.bind(supportController))
 .get('/support/open-tickets', authMiddleware, supportController.getAllOpenTickets.bind(supportController))
